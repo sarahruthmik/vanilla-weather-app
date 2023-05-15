764c35a7e1b25de7d4ccdf33b56303f2c3c31f9a
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-05-15 12:17:30 -0400

    Added basic layout of current conditions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-alert("Hello");
+function formatDate(date) {
+  let dateTime = document.querySelector("#date-time");
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+let dateElement = document.querySelector("#date-time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
