57c0dca2c03aba13588a5473037fcb565770a716
sarahruthmik <128433888+sarahruthmik@users.noreply.github.com>
2023-05-18 12:06:26 -0400

    Showing current temperature and conditions

---
+++ src/app.js
@@ -0,0 +1,19 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  console.log(response.data);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "b7a14ca0987ft690bf2094b4a086a36o";
+let city = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
